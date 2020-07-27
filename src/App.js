7843502e2b5e7d212ecbcd5e64cae0dace04fbe4
Paul Gildea <paul.gildea@live.com>
2020-07-27 11:23:47 -0700

    Added a theme, updated some icons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,77 @@
 import React from 'react';
 import './App.css';
 import Directory  from './directory';
-import { SearchBox, initializeIcons, Text, Stack, Link } from '@fluentui/react';
+import { DarkCustomizations } from '@uifabric/theme-samples';
+import { SearchBox, initializeIcons, Text, Stack, Link, Fabric, Customizer, createTheme, Customizations, Toggle } from '@fluentui/react';
 
 const appTokens = {
   padding: 10
 }
 
+const darkTheme = createTheme({
+  palette: {
+    themePrimary: '#0d83de',
+    themeLighterAlt: '#010509',
+    themeLighter: '#021523',
+    themeLight: '#042743',
+    themeTertiary: '#084f85',
+    themeSecondary: '#0c74c3',
+    themeDarkAlt: '#238fe1',
+    themeDark: '#429fe6',
+    themeDarker: '#70b7ec',
+    neutralLighterAlt: '#1a1919',
+    neutralLighter: '#232222',
+    neutralLight: '#323030',
+    neutralQuaternaryAlt: '#3b3939',
+    neutralQuaternary: '#434141',
+    neutralTertiaryAlt: '#636060',
+    neutralTertiary: '#c8c8c8',
+    neutralSecondary: '#d0d0d0',
+    neutralPrimaryAlt: '#dadada',
+    neutralPrimary: '#ffffff',
+    neutralDark: '#f4f4f4',
+    black: '#f8f8f8',
+    white: '#0f0f0f',
+  }});
+
+  const lightTheme = createTheme({
+    palette: {
+      themePrimary: '#0078d4',
+      themeLighterAlt: '#eff6fc',
+      themeLighter: '#deecf9',
+      themeLight: '#c7e0f4',
+      themeTertiary: '#71afe5',
+      themeSecondary: '#2b88d8',
+      themeDarkAlt: '#106ebe',
+      themeDark: '#005a9e',
+      themeDarker: '#004578',
+      neutralLighterAlt: '#faf9f8',
+      neutralLighter: '#f3f2f1',
+      neutralLight: '#edebe9',
+      neutralQuaternaryAlt: '#e1dfdd',
+      neutralQuaternary: '#d0d0d0',
+      neutralTertiaryAlt: '#c8c6c4',
+      neutralTertiary: '#a19f9d',
+      neutralSecondary: '#605e5c',
+      neutralPrimaryAlt: '#3b3a39',
+      neutralPrimary: '#323130',
+      neutralDark: '#201f1e',
+      black: '#000000',
+      white: '#ffffff',
+    }});
+
+const searchStyles = {
+  root: {
+    width: '320px',
+    maxWidth: '320px',
+    margin: 'auto'
+  }
+}
+
 function App() {
 
   initializeIcons();
+  Customizations.applySettings({theme: darkTheme});
 
   const headers = Directory.schema.map((header, i) => 
     <th>{header}</th> );
@@ -26,18 +88,22 @@ function App() {
 
   return (
     <div className="App">
-      <Stack tokens={{appTokens}}>
-        <Text variant={'large'}>Fluent UI Directory</Text>
-        <SearchBox placeholder="Search" onSearch={newValue => console.log('value is ' + newValue)} />
-        <table>
-          <thead>
-            {headers}
-          </thead>
-          <tbody>
-            {listing}
-          </tbody>
-        </table>
-      </Stack>
+      <Fabric applyThemeToBody>
+        <Stack tokens={appTokens}>
+          <Stack horizontal>
+            <Text variant={'large'}>Fluent UI Directory</Text>
+          </Stack>
+          <SearchBox styles={searchStyles} placeholder="Search" onSearch={newValue => console.log('value is ' + newValue)} />
+          <table>
+            <thead>
+              {headers}
+            </thead>
+            <tbody>
+              {listing}
+            </tbody>
+          </table>
+        </Stack>
+      </Fabric>
     </div>
   );
 }
