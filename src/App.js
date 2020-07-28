a6fcbd6d90753280665238970134380cfa8dd370
Paul Gildea <paul.gildea@live.com>
2020-07-28 15:00:39 -0700

    Added basic filtering of the listing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Directory  from './directory';
 import { SearchBox, initializeIcons, Text, Stack, Link, Fabric, createTheme, Customizations} from '@fluentui/react';
@@ -45,11 +45,19 @@ function App() {
 
   initializeIcons();
   Customizations.applySettings({theme: darkTheme});
+  const [listing, setListing] = useState(Directory.listing);
+  const filterResults = (ev, newValue) => {
+    const retValue = Directory.listing.filter((item) => {
+      const lowerCaseValue = newValue.toLowerCase();
+      return item.name.toLowerCase().includes(lowerCaseValue); 
+    });
+    setListing(retValue);
+  };
 
   const headers = Directory.schema.map((header, i) => 
     <th>{header}</th> );
   
-  const listing = Directory.listing.map((library, i) => 
+  const directoryListing = listing.map((library, i) => 
     <tr>
       <td>{library.name}</td>
       <td>{library.type}</td>
@@ -67,13 +75,15 @@ function App() {
           <Stack horizontal>
             <Text variant={'large'}>Fluent UI Directory</Text>
           </Stack>
-          <SearchBox styles={searchStyles} placeholder="Search" onSearch={newValue => console.log('value is ' + newValue)} />
+          <SearchBox iconProps={{iconName: 'Filter'}} styles={searchStyles} placeholder="Filter" onChange={filterResults} onClear={(ev) => {
+            setListing(Directory.listing);
+          }} />
           <table>
             <thead>
               {headers}
             </thead>
             <tbody>
-              {listing}
+              {directoryListing}
             </tbody>
           </table>
         </Stack>
